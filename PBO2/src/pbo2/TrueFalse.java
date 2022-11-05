32a2509719f94724bf187c9e2dfa2bd898ec1237
fiarisyaifahidayati <117350671+fiarisyaifahidayati@users.noreply.github.com>
2022-11-05 11:13:58 +0800

    Add files via upload

---
+++ PBO2/src/pbo2/TrueFalse.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo2;
+
+/**
+ *
+ * @author ACER
+ */
+public class TrueFalse {
+    public static void main(String[] args) { 
+        String nama = "Fiari";
+        int nilai = 99;
+        boolean lulus = true; 
+        System.out.println("Nama : "+nama); 
+        System.out.println("Nilai : "+nilai); 
+        System.out.println("Status : "+lulus);
+    }
+}
